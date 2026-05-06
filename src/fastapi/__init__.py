ee5fcb3ea42ddacc30228b6c39e1cb6e43ce8dc4
gokhanaydinn16 <gokhanaydinn16@gmail.com>
2026-05-06 08:22:25 +0300

    Export Header and Cookie from FastAPI shim

--- src/fastapi/__init__.py
+++ src/fastapi/__init__.py
@@ -1,3 +1,3 @@
-from .app import FastAPI, HTTPException, Query, WebSocket, WebSocketDisconnect
+from .app import Cookie, FastAPI, Header, HTTPException, Query, WebSocket, WebSocketDisconnect
 
-__all__ = ["FastAPI", "HTTPException", "Query", "WebSocket", "WebSocketDisconnect"]
+__all__ = ["Cookie", "FastAPI", "Header", "HTTPException", "Query", "WebSocket", "WebSocketDisconnect"]
